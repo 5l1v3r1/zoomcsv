aaec6ce08f85fec44732aa457330549f6bef3462
Robbie Morrison <robbie.morrison@posteo.de>
2020-05-21 19:23:49 +0200

    added usage message

--- run.cmd
+++ run.cmd
@@ -3,6 +3,16 @@
 
 target="participants_123456789.csv"
 utility="zoompart.py"
+
+case "$1" in
+    --help|--hel|--he|--h|-help|-hel|-he|-h|"-?")
+        shift
+        echo "   usage: run without options or arguments"
+        echo " purpose: issue a test command for $utility"
+        exit 0
+        ;;
+esac
+
 $utility \
   --verbose \
   --dat-file \
@@ -10,3 +20,5 @@ $utility \
   --cutoff=20 \
   --title="Test plot" \
   $target
+
+# end of file
